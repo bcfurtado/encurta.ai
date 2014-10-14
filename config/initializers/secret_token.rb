120d381638cae94f27131eb21d87062a6b9c86f7
Bruno Furtado <brunofurtadoc@gmail.com>
2014-10-14 03:59:19 -0300

    project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EncurtaAi::Application.config.secret_key_base = 'cb805b6b761b19707d289679bab3bcf0c04a0cef5182be7798567aef9372164f27e8034b34473a5d129e8aeafc2315029096a1cf94e80d92bb600c5bae8432f4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EncurtaAi::Application.config.session_store :cookie_store, key: '_encurta_ai_session'
